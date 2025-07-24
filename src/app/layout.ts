ad6848b1ec3294c950771475196b7b6cfde0d057
andersseen <andriipap01@gmail.com>
2025-07-24 21:52:29 +0200

    Update Layout

--- src/app/layout.ts
+++ src/app/layout.ts
@@ -1,34 +1,100 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, Renderer2, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import {
+  MatSidenav,
+  MatSidenavContainer,
+  MatSidenavContent,
+} from '@angular/material/sidenav';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import Footer from '@components/footer';
 import Navbar from '@components/navbar';
 import routesConfig from '@data/routes.json';
 
 @Component({
   selector: 'app-layout',
-  imports: [Footer, Navbar],
-
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatSidenav,
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatButton,
+    Footer,
+    Navbar,
+  ],
   template: `
-    <div class="min-h-screen transition-colors duration-300">
+    <section
+      class="min-h-screen transition-colors duration-300 overflow-hidden"
+    >
       <!-- Navigation -->
       <app-navbar
         [routes]="data().routes"
         [projectName]="data().metadata.projectName"
+        (hamburgerClick)="sidenav.toggle()"
       />
       <!-- Main Content -->
-      <main class="container mx-auto px-4 py-8">
-        <ng-content />
-      </main>
+      <mat-sidenav-container>
+        <mat-sidenav
+          (closedStart)="sidenavClosed()"
+          (openedStart)="sidenavOpened()"
+          #sidenav
+          position="end"
+          mode="over"
+          class="sidebar-container"
+        >
+          <aside class="mt-8">
+            <nav class="flex flex-col gap-4">
+              @for (route of data().routes; track route.path) {
+              <a
+                [routerLink]="route.path"
+                routerLinkActive
+                #rla="routerLinkActive"
+              >
+                <button
+                  [matButton]="rla.isActive ? 'tonal' : 'elevated'"
+                  (click)="sidenav.toggle()"
+                  class="w-full"
+                >
+                  {{ route.name }}
+                </button>
+              </a>
+              }
+            </nav>
+          </aside>
+        </mat-sidenav>
 
-      <!-- Footer -->
-      <app-footer />
-    </div>
+        <mat-sidenav-content>
+          <main class="container mx-auto px-4 py-8">
+            <ng-content />
+            <!-- Footer -->
+            <app-footer />
+          </main>
+        </mat-sidenav-content>
+      </mat-sidenav-container>
+    </section>
   `,
+  styles: [
+    `
+      .sidebar-container {
+        height: calc(100dvh - var(--mat-toolbar-standard-height));
+      }
+    `,
+  ],
 })
 export default class Layout {
+  #renderer = inject(Renderer2);
+
   public routesConfig = signal(routesConfig);
 
   public data = computed(() => ({
     routes: this.routesConfig().routes,
     metadata: this.routesConfig().metadata,
   }));
+
+  sidenavClosed() {
+    this.#renderer.removeClass(document.body, 'preventScrolling');
+  }
+  sidenavOpened() {
+    this.#renderer.addClass(document.body, 'preventScrolling');
+  }
 }
