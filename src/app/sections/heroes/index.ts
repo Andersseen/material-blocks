7a9f30545b9845e86375d7a00263fcaac354bb65
andersseen <andriipap01@gmail.com>
2025-07-27 19:33:35 +0200

    Add NavigationState

--- src/app/sections/heroes/index.ts
+++ src/app/sections/heroes/index.ts
@@ -1,11 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import BlocksList from '@components/block-list';
 import { heroSectionsData } from './data';
 
 @Component({
   selector: 'page-blocks-list',
   imports: [BlocksList],
-
   template: ` <blocks-list [data]="data" />`,
 })
 export default class HeroesPage {
