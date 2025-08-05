1f1041a231ca24615767f954a48e1a539fdfd7bf
andersseen <andriipap01@gmail.com>
2025-08-05 15:26:42 +0200

    Update server.routes

--- src/server/routes.server.ts
+++ src/server/routes.server.ts
@@ -87,6 +87,14 @@ export const serverRoutes: ServerRoute[] = [
       return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }];
     },
   },
+  {
+    path: 'sections/ecommerce/:id',
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Client,
+    async getPrerenderParams() {
+      return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }];
+    },
+  },
 
   {
     path: '**',
