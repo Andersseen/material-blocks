43f7ab56b5868efaa5bfa223a729ed96218b0a94
andersseen <andriipap01@gmail.com>
2025-08-07 19:12:39 +0200

    Update BDCode

--- src/components/block-details/code.ts
+++ src/components/block-details/code.ts
@@ -1,4 +1,4 @@
-import { Component, input, model } from '@angular/core';
+import { Component, input, model, signal } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
@@ -9,41 +9,54 @@ import { Highlight } from 'ngx-highlightjs';
 @Component({
   selector: 'bd-code',
   imports: [MatTabGroup, MatTab, MatIconButton, MatIcon, MatTooltip, Highlight],
-  template: ` <mat-tab-group
-    [(selectedIndex)]="selectedTabIndex"
-    animationDuration="200ms"
-  >
-    @for (view of blockViews(); track $index) {
-    <mat-tab [label]="view.label">
-      <!-- Code Content -->
-      <div class="relative">
-        <div class="absolute top-3 right-3 z-10">
-          <button
-            matIconButton
-            size="small"
-            class="!text-muted-foreground hover:!text-foreground !bg-background/80 backdrop-blur-sm !shadow-sm"
-            matTooltip="Copy"
-            (click)="copyCode(view.content)"
-          >
-            <mat-icon class="!text-base">content_copy</mat-icon>
-          </button>
-        </div>
+  template: `
+    <mat-tab-group
+      [(selectedIndex)]="selectedTabIndex"
+      animationDuration="200ms"
+    >
+      @for (view of blockViews(); track $index) {
+      <mat-tab [label]="view.label">
+        <!-- Code Content -->
+        <div class="relative">
+          <div class="absolute top-3 right-3 z-10">
+            <button
+              matIconButton
+              size="small"
+              class="relative"
+              matTooltip="Copy"
+              (click)="copyCode(view.content)"
+            >
+              <mat-icon class="!text-base">content_copy</mat-icon>
+              @if (copied()) {
+              <span
+                class="absolute -left-16 top-1/2 -translate-y-1/2
+           text-xs rounded px-2 py-1  animate-fade-out"
+              >
+                Copied!
+              </span>
+              }
+            </button>
+          </div>
 
-        <pre
-          class="bg-card text-card-foreground p-4 overflow-x-auto text-sm leading-relaxed"
-        >
+          <pre
+            class="bg-card text-card-foreground p-4 overflow-x-auto text-sm leading-relaxed"
+          >
         <code [highlight]="view.content" [language]="view.language|| 'typescript'"></code>
       </pre>
-      </div>
-    </mat-tab>
-    }
-  </mat-tab-group>`,
+        </div>
+      </mat-tab>
+      }
+    </mat-tab-group>
+  `,
 })
 export default class BDCode {
   public blockViews = input<BlockView[]>();
   public selectedTabIndex = model(0);
+  public copied = signal(false);
 
   copyCode(content: string) {
     navigator.clipboard.writeText(content);
+    this.copied.set(true);
+    setTimeout(() => this.copied.set(false), 1000);
   }
 }
